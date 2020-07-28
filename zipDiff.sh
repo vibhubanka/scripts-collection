197c1609352f64370d9236832dcd8c6f8a259f13
Vibhu Banka <v.banka93@gmail.com>
2020-07-28 22:50:36 +0530

    Refactors code, adds minor tweaks, adds prompt before zipping

--- zipDiff.sh
+++ zipDiff.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 
-git fetch --all
 if [ -z "$1" ]; then
     echo "Please pass zip file name as parameter!"
     exit 1
 fi
-echo "Copy and Paste the source commit ID and press ENTER. Leave empty for HEAD commit."
-read -p "Commit ID: " commitId
 
-if [ -z "$commitId" ]; then
+git fetch -q --all && echo "Updated objects and refs." || echo "Could not update objects and refs."
+
+echo "Copy and Paste the source commit ID and press ENTER. Leave empty for HEAD commit."
+read -p "Commit ID: " commit_id
+
+if [ -z "$commit_id" ]; then
   echo "Using HEAD commit as default commit ID."
-  commitId=`git rev-parse HEAD`
+  commit_id=`git rev-parse HEAD`
 fi
 
-if ! $(git cat-file -e $commitId^{commit} >& /dev/null) ; then
+if ! $(git cat-file -e $commit_id^{commit} >& /dev/null) ; then
   echo "Invalid commit ID. Please rerun the script and try again!"
   exit 1
 fi
 
-cd $(git rev-parse --show-toplevel)
+git_root=$(git rev-parse --show-toplevel)
+cd $git_root
 
-FILES=`git diff --name-only "origin/master"..."$commitId"`
+FILES=`git diff --name-only "origin/master"..."$commit_id"`
 PARAM=""
-echo "Zipping below files..."
+echo "Below files will be zipped..."
 for f in $FILES; do
   echo "> $f"
   PARAM=`echo "$PARAM$f "`
 done
-zip -q -r $1 $PARAM
+
+read -p "Continue? (Y / N): " response
+if [[ $response == [yY] ]] ; then
+  zip -q -r $1 $PARAM && echo "Created File: $git_root/$1" || echo "Could not create zip file" && exit -1
+else
+  echo ""
+fi
 
 cd $OLDPWD
+exit 0
